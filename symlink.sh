dfa9d589e56eeaeb988e7ee982eec92ee6cdf0d1
Christian Heller <c.heller@plomlompom.de>
2015-05-03 05:03:16 +0200

    Fix root detection bug in symlink script.

--- symlink.sh
+++ symlink.sh
@@ -7,7 +7,7 @@ dir_root=~/config/dotfiles_root
 for file in `ls $dir`; do
     ln -fs $dir/$file ~/.$file
 done
-if [ "$(id -u)" -ne "0" ]; then
+if [ "$(id -u)" -eq "0" ]; then
     for file in `ls $dir_root`; do
         ln -fs $dir_root/$file ~/.$file
     done
