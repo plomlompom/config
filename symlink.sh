9dbce2b880b7d32c1a263a514dec5c3fa067dc12
Christian Heller <c.heller@plomlompom.de>
2015-08-08 04:11:22 +0200

    Always overwrite destination files in symlink script.

--- symlink.sh
+++ symlink.sh
@@ -8,11 +8,11 @@ dir_root=~/config/dotfiles_root
 homedir=`echo ~`
 find ~ -lname $homedir'/config/*' -delete
 for file in `ls $dir_user_minimal`; do
-    ln -s $dir_user_minimal/$file ~/.$file
+    ln -fs $dir_user_minimal/$file ~/.$file
 done
 if [ "$1" = "thinkpad" ]; then
     for file in `ls $dir_user_thinkpad`; do
-        ln -s $dir_user_thinkpad/$file ~/.$file
+        ln -fs $dir_user_thinkpad/$file ~/.$file
     done
 fi
 if [ "$(id -u)" -eq "0" ]; then
