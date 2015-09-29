8d972de8b8c9de9ba5fc19c09b2e2189f972b3c1
Christian Heller <c.heller@plomlompom.de>
2015-09-29 04:36:45 +0200

    Fix irssi screen connection wait.

--- other/screen-irssi.sh
+++ other/screen-irssi.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+# Wait until online.
+online=0
+while [ $online -eq 0 ]; do
+    ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && \
+    online=1 || online=0
+done
+echo 1
+
+# Start irssi in shell in screen.
 screen -d -m -S irssi
 screen -S irssi -X stuff 'irssi\n'
