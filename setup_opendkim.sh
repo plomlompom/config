ec7cf5e045fbdc6f415a47851e15caf593dcaa46
Christian Heller <c.heller@plomlompom.de>
2015-10-09 14:37:07 +0200

    Improve opendkim setup script.

--- setup_opendkim.sh
+++ setup_opendkim.sh
@@ -9,37 +9,37 @@ if [ ! "$(id -u)" -eq "0" ]; then
     exit
 fi
 
-apt-get -y install opendkim opendkim-tools
-cp ~/config/systemfiles/opendkim.conf /etc/opendkim.conf
+apt-get -y install opendkim
 
-if [ -f /etc/opendkim/dkim.key ]; then
-    cp /etc/opendkim/dkim.key /etc/opendkim/dkim.key~
-fi
-
-sed -r -i 's/^#Selector .*$/Selector '$selector'/' /etc/opendkim.conf
-
-mkdir -p /etc/opendkim
 if [ -n $file ] || [ ! -f $file ]; then
+    apt-get -y install opendkim-tools
     opendkim-genkey -d plomlompom.com -s $selector
-    mv "$selector".private /etc/opendkim/dkim.key
+    apt-get --purge autoremove opendkim-tools
+    set +x
+    echo
+    echo 'Generated key file at '$selector'.private.'
+    echo 'APPLY the content of '$selector'.txt below to your DNS record.'
+    echo 'AFTER the waiting time for DNS propagation RERUN this script with' \
+          'the key file as SECOND parameter (still use selector as first one).'
+    echo
+    cat $selector.txt
 else
+    cp ~/config/systemfiles/opendkim.conf /etc/opendkim.conf
+    sed -r -i 's/^#Selector .*$/Selector '$selector'/' /etc/opendkim.conf
+    mkdir -p /etc/opendkim
+    if [ -f /etc/opendkim/dkim.key ]; then
+        cp /etc/opendkim/dkim.key /etc/opendkim/dkim.key~
+    fi
     cp $file /etc/opendkim/dkim.key
-fi
-
-cp ~/config/systemfiles/main.cf /etc/postfix/main.cf
-cat >> /etc/postfix/main.cf << EOF
+    cp ~/config/systemfiles/main.cf /etc/postfix/main.cf
+    cat >> /etc/postfix/main.cf << EOF
 
 # Use opendkim at given port as mail filter.
 non_smtpd_milters = inet:localhost:12301
-smtpd_milters = inet:localhost:12301
 EOF
-service postfix restart
-service opendkim restart
-
-echo 'TAKE NOTE:'
-if [ -f $selector.txt ]; then
-    echo 'Apply the content of '$selector'.txt to your DNS record!'
-    cat $selector.txt 
-else
-    echo 'Make sure the DKIM TXT entry in your DNS record matches!'
+    service opendkim restart
+    service postfix restart
+    set +x
+    echo
+    echo 'Ensure the DKIM TXT entry in your DNS record matches!'
 fi
