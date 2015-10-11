822fe8e489823bb5c1e6a8a90ebda2ba7e35cf9f
Christian Heller <c.heller@plomlompom.de>
2015-10-11 20:35:08 +0200

    On server re-start, send reminder mail to re-identify via NickServ.

--- bin/screen-irssi.sh
+++ bin/screen-irssi.sh
@@ -11,3 +11,6 @@ echo 1
 # Start irssi in shell in screen.
 screen -d -m -S irssi
 screen -S irssi -X stuff 'irssi\n'
+
+# Send mail to remind user to re-identify to NickServ.
+~/config/bin/simplemail.sh ~/config/mails/irssi_identify_reminder
