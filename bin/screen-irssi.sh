ce8e37989a146af86ec093c749e02fa2d82a434d
Christian Heller <c.heller@plomlompom.de>
2015-10-12 02:47:27 +0200

    Fix race condition in irssi screen script.

--- bin/screen-irssi.sh
+++ bin/screen-irssi.sh
@@ -6,10 +6,13 @@ while [ $online -eq 0 ]; do
     ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && \
     online=1 || online=0
 done
-echo 1
 
-# Start irssi in shell in screen.
+# Start shell in screen, wait until it's created, then start irssi in it.
 screen -d -m -S irssi
+screen_available=0
+while [ $screen_available -eq 0 ]; do
+    screen_available=`screen -list | grep irssi | wc -l`
+done
 screen -S irssi -X stuff 'irssi\n'
 
 # Send mail to remind user to re-identify to NickServ.
