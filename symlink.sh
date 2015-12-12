62c0fa84083528ad1d5f01569689230263aab8d2
Christian Heller <c.heller@plomlompom.de>
2015-12-12 12:12:48 +0100

    Don't symlink user dotfiles into root home.

--- symlink.sh
+++ symlink.sh
@@ -12,20 +12,21 @@ find ~ -lname $homedir'/config/*' -delete
 for file in `ls $dir_minimal`; do
     ln -fs $dir_minimal/$file ~/.$file
 done
-for file in `ls $dir_user_minimal`; do
-    ln -fs $dir_user_minimal/$file ~/.$file
-done
-if [ "$1" = "thinkpad" ]; then
-    for file in `ls $dir_user_thinkpad`; do
-        ln -fs $dir_user_thinkpad/$file ~/.$file
-    done
-elif [ "$1" = "server" ]; then
-    for file in `ls $dir_user_server`; do
-        ln -fs $dir_user_server/$file ~/.$file
-    done
-fi
 if [ "$(id -u)" -eq "0" ]; then
     for file in `ls $dir_root`; do
         ln -fs $dir_root/$file ~/.$file
     done
+else
+    for file in `ls $dir_user_minimal`; do
+        ln -fs $dir_user_minimal/$file ~/.$file
+    done
+    if [ "$1" = "thinkpad" ]; then
+        for file in `ls $dir_user_thinkpad`; do
+            ln -fs $dir_user_thinkpad/$file ~/.$file
+        done
+    elif [ "$1" = "server" ]; then
+        for file in `ls $dir_user_server`; do
+            ln -fs $dir_user_server/$file ~/.$file
+        done
+    fi
 fi
