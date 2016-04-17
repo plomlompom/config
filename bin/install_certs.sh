a974ad102ce54aa9e368c3ef43293f1410e2a2ee
Christian Heller <c.heller@plomlompom.de>
2016-04-17 23:53:59 +0200

    Minor fixes.

--- bin/install_certs.sh
+++ bin/install_certs.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-~/letsencrypt-auto certonly --webroot -w /var/www/html -d dump.plomlompom.com 
+~/letsencrypt/letsencrypt-auto certonly --webroot -w /var/www/html -d dump.plomlompom.com
