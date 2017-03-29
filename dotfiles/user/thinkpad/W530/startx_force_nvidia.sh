8cb8b27b86315b95fac04800be36ce78f8aead97
Christian Heller <c.heller@plomlompom.de>
2017-03-29 02:35:46 +0200

    Provide scripting for W530 to startx with forced NVidia GPU usage.

---
+++ dotfiles/user/thinkpad/W530/startx_force_nvidia.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Undo bumblebee setup.
+sudo service bumblebeed stop
+sudo modprobe nvidia-drm
+sudo update-alternatives --set glx /usr/lib/nvidia
+
+# Use special xorg.conf and pass NVIDIA_DIRECT directive to .xinitrc.
+NVIDIA_DIRECT=1 startx -- -config xorg.conf.alt
+
+# Recreate bumblebee setup.
+sudo service bumblebeed start
+sudo update-alternatives --auto glx 
