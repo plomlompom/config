c5438baf5d0f7dbdbf7c172a218a5b1bbdea6f84
Christian Heller <c.heller@plomlompom.de>
2017-03-29 03:01:07 +0200

    Improve on previous commit.

--- dotfiles/user/thinkpad/W530/startx_force_nvidia.sh
+++ dotfiles/user/thinkpad/W530/startx_force_nvidia.sh
@@ -6,7 +6,7 @@ sudo modprobe nvidia-drm
 sudo update-alternatives --set glx /usr/lib/nvidia
 
 # Use special xorg.conf and pass NVIDIA_DIRECT directive to .xinitrc.
-NVIDIA_DIRECT=1 startx -- -config xorg.conf.alt
+NVIDIA_DIRECT=1 startx -- -config xorg.conf.forced_nvidia
 
 # Recreate bumblebee setup.
 sudo service bumblebeed start
