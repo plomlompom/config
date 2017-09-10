a8b7d1a46c9234473931171b20e3ac805f43d2c1
Christian Heller <c.heller@plomlompom.de>
2017-09-10 12:00:18 +0200

    Fix minor bugs with new ansible tree.

--- ansible/run_root_new.sh
+++ ansible/run_root_new.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i 'localhost,'  -e system_name=X200s -c local config.yml
+ansible-playbook -i 'localhost,'  -e system_name=X200s -c local config_new.yml
--- ansible/run_user_new.sh
+++ ansible/run_user_new.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i 'localhost,'  -e system_name=X200s -c local user.yml
+ansible-playbook -i 'localhost,'  -e system_name=X200s -c local user_new.yml
