c23144e7c48a68a277dc49203a278a7fe9936a36
Christian Heller <c.heller@plomlompom.de>
2018-11-14 19:21:45 +0100

    Add setup_server.sh script.

---
+++ all_new_2018/setup_server.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Next setup steps for a server whose login policy has just been set from
+# the outside via ./init_user_and_keybased_login.sh.
+set -e
+
+# Adapt /etc/ to our needs by symlinking into ./linkable_etc_files. This
+# will set basic configurations affecting following steps, such as setup
+# of APT and the locale selection, so needs to be right at the beginning.
+./symlink_etc.sh all server
+
+# Ensure package installation state as defined by what packages are
+# defined as required by Debian policy and by settings in ./apt-mark/.
+apt update
+./install_for_target.sh all server
+./purge_nonrequireds.sh all server
+
+# Only upgrade after reducing the system to the desired minimum, so that
+# we don't need to get more data than necessary.
+apt -y dist-upgrade
+
+# If we have not yet set the shell for user plom, ensure it here.
+usermod -s /bin/bash plom
