85b455bd3c61b03025860de830194a8501d0ddbf
Christian Heller <c.heller@plomlompom.de>
2018-11-23 14:01:48 +0100

    WIP.

--- all_new_2018/setup_mail.sh
+++ all_new_2018/setup_mail.sh
@@ -2,9 +2,10 @@
 set -e
 
 echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
-echo "postfix postfix/mailname string $(hostname -f)" | debconf-set-selections
-apt install postfix
+#echo "postfix postfix/mailname string $(hostname -f)" | debconf-set-selections
+echo "$(hostname -f)" > /etc/mailname
+apt install -y postfix
 mkdir -p /etc/dovecot/conf.d/
 echo "ssl_cert = </etc/letsencrypt/live/$(hostname -f)/fullchain.pem" > /etc/dovecot/conf.d/99-ssl-certs.conf
 echo "ssl_key = </etc/letsencrypt/live/$(hostname -f)/privkey.pem" >> /etc/dovecot/conf.d/99-ssl-certs.conf
-apt install dovecot-imapd
+apt install -y dovecot-imapd
