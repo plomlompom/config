4bd4edbb46ec15969f7de6acc5b24dd60275d0bb
Christian Heller <c.heller@plomlompom.de>
2018-12-05 01:13:52 +0100

    Add git repo serving/browsing to web server.

--- all_new_2018/setup_web.sh
+++ all_new_2018/setup_web.sh
@@ -3,5 +3,6 @@ set -e
 
 ./hardlink_etc.sh web
 sed -i "s/REPLACE_fqdn_ECALPER/$(hostname -f)/g" /etc/nginx/nginx.conf
-apt -y -o Dpkg::Options::=--force-confold install nginx
+sed -i "s/REPLACE_fqdn_ECALPER/$(hostname -f)/g" /etc/gitweb.conf
+apt -y -o Dpkg::Options::=--force-confold install nginx gitweb fcgiwrap
 iptables-restore /etc/iptables/rules.v4
