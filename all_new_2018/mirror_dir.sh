75be7235258390736996362e04beda1650fc1b4a
Christian Heller <c.heller@plomlompom.de>
2018-12-15 00:21:14 +0100

    Add a script to mirror directory trees.

---
+++ all_new_2018/mirror_dir.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+
+if [ $# -lt 2 ]; then
+    echo "Need server and directory as arguments."
+    false
+fi
+server=$1
+dir=$2
+path_package=/tmp/delete.tar
+
+eval `ssh-agent`
+ssh-add
+cd
+ssh plom@"${server}" "cd \"${dir}\" && tar cf ${path_package} ."
+scp plom@"${server}":"${path_package}" .
+mkdir -p "${dir}"
+cd "${dir}"
+tar xf "${path_package}"
+cd
+rm "${path_package}"
+ssh plom@"${server}" rm "${path_package}"
