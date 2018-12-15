7823df99c3e51c9591bf2c5a2a0071163c6b4832
Christian Heller <c.heller@plomlompom.de>
2018-12-15 19:40:01 +0100

    Improve play server setup script.

--- all_new_2018/setup_scripts/setup_play.sh
+++ all_new_2018/setup_scripts/setup_play.sh
@@ -13,19 +13,22 @@ setup_scripts_dir="${config_tree_prefix}/setup_scripts"
 cd "${setup_scripts_dir}"
 
 ./setup_sendonly.sh
+
 ./add_encryption_key.sh "${gpg_key}"
-config_tree_prefix="${HOME}/config/all_new_2018/"
-./hardlink_etc.sh play
 apt -y install weechat screen vim
-cp "${config_tree_prefix}user_files/encrypter.sh" /home/plom/
+
+# Link and copy over files.
+./hardlink_etc.sh play
+cp "${config_tree_prefix}/user_files/encrypter.sh" /home/plom/
 chown plom:plom /home/plom/encrypter.sh
-cp "${config_tree_prefix}user_files/weechat-wrapper.sh" /home/plom/
+cp "${config_tree_prefix}/user_files/weechat-wrapper.sh" /home/plom/
 chown plom:plom /home/plom/weechat-wrapper.sh
-cp "${config_tree_prefix}user_files/weechatrc" /home/plom/.weechatrc
+cp "${config_tree_prefix}/user_files/weechatrc" /home/plom/.weechatrc
 chown plom:plom /home/plom/.weechatrc
 apt -y install screen
 echo "$gpg_key" > /home/plom/.encrypt_target
 chown plom:plom /home/plom/.encrypt_target
-# TODO: We may remove dirmngr here if only this script installed it.
+
+# Start encrypt_chatlogs job.
 systemctl daemon-reload
 systemctl start encrypt_chatlogs.timer
