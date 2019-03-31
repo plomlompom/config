57e87507056dc3a04dc472e1820a7bea4154c887
Christian Heller <c.heller@plomlompom.de>
2019-04-01 01:29:29 +0200

    Fix previous commit's bugs.

--- buster/setup_scripts/setup_eeepc.sh
+++ buster/setup_scripts/setup_eeepc.sh
@@ -11,4 +11,4 @@ config_tree_prefix="${HOME}/config/buster"
 setup_scripts_dir="${config_tree_prefix}/setup_scripts"
 cd "${setup_scripts_dir}"
 ./setup.sh "${hostname}" ""
-./instal_for_target.sh eeepc
+./install_for_target.sh eeepc
