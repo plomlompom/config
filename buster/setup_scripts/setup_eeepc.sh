c5c421b0784b2cca5e1dbc064690536e734f3c11
Christian Heller <c.heller@plomlompom.de>
2019-04-06 02:14:06 +0200

    To Buster setup, add user initialization.

--- buster/setup_scripts/setup_eeepc.sh
+++ buster/setup_scripts/setup_eeepc.sh
@@ -13,3 +13,9 @@ cd "${setup_scripts_dir}"
 ./setup.sh "${hostname}" ""
 ./copy_dirtree.sh "${config_tree_prefix}/etc_files" "" eeepc
 ./install_for_target.sh eeepc
+
+if [ ! -d "/home/plom" ]; then
+    adduser --disabled-password --gecos plom
+    su -c "cd && git clone https://plomlompom.com/repos/clone/config" plom
+    su -c "~/config/buster/setup_scripts/copy_dirtree.sh ~/config/buster/home_files ~ eeepc" plom
+fi
