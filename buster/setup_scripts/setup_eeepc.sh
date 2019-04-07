f91a250c42c9baf6711962bfaea6bacdc07889d0
Christian Heller <c.heller@plomlompom.de>
2019-04-07 23:19:17 +0200

    For Buster EeePC setup, install firefox 66 instead of ESR.

--- buster/setup_scripts/setup_eeepc.sh
+++ buster/setup_scripts/setup_eeepc.sh
@@ -14,6 +14,17 @@ cd "${setup_scripts_dir}"
 ./copy_dirtree.sh "${config_tree_prefix}/etc_files" "" eeepc
 ./install_for_target.sh eeepc
 
+url_firefox="https://ftp.mozilla.org/pub/firefox/releases/66.0/linux-x86_64/en-US/firefox-66.0.tar.bz2"
+wget "${url_firefox}"
+mv firefox-66.0.tar.bz2 /opt/
+cd /opt/
+tar xf firefox-66.0.tar.bz2
+rm firefox-66.0.tar.bz2
+ln -s /opt/firefox/firefox /usr/local/bin/
+update-alternatives --install /usr/bin/x-www-browser x-www-browser /opt/firefox/firefox 200
+update-alternatives --set x-www-browser /opt/firefox/firefox
+cd "${setup_scripts_dir}"
+
 ./copy_dirtree.sh "${config_tree_prefix}/home_files" "/root" minimal root
 if [ ! -d "/home/plom" ]; then
     adduser --disabled-password --gecos "" plom
