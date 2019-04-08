10d09e174e66ea4409a520ff48795d6e9758fb8b
Christian Heller <c.heller@plomlompom.de>
2019-04-09 01:15:53 +0200

    Enable pre-installed Firefox extensions by default.

---
+++ buster/etc_files/eeepc/opt/firefox/defaults/pref/autoconfig.js
@@ -0,0 +1,4 @@
+// We set up AutoConfig according to <https://support.mozilla.org/en-US/kb/customizing-firefox-using-autoconfig>, see firefox.cfg comments on why we need it
+pref("general.config.filename", "firefox.cfg");
+pref("general.config.obscure_value", 0);
+
