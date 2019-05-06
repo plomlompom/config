203ed9892e81e1df5137930a4b367dd7bf22a64c
Christian Heller <c.heller@plomlompom.de>
2019-05-06 21:01:47 +0200

    Ensure local clonings of public repos.

--- buster/setup_scripts/setup_home_eeepc.sh
+++ buster/setup_scripts/setup_home_eeepc.sh
@@ -4,14 +4,27 @@ set -e
 public_repos_dir="${HOME}/public_repos"
 config_tree_prefix="${public_repos_dir}/config/buster"
 setup_scripts_dir="${config_tree_prefix}/setup_scripts"
+repos_list_file="${public_repos_dir}/repos"
+
+ensure_repo() {
+    repo_name="${1}"
+    if [ ! -d "${HOME}/${public_repos_dir}/${repo_name}" ]; then
+        cd "${public_repos_dir}"
+        git clone https://plomlompom.com/repos/clone/${repo_name}
+    fi
+}
 
 cd
 mkdir -p "${public_repos_dir}"
-if [ ! -d "${HOME}/${public_repos_dir}/config" ]; then
-    cd "${public_repos_dir}"
-    git clone https://plomlompom.com/repos/clone/config
-fi
+ensure_repo config
+#if [ ! -d "${HOME}/${public_repos_dir}/config" ]; then
+#    cd "${public_repos_dir}"
+#    git clone https://plomlompom.com/repos/clone/config
+#fi
 cd "${setup_scripts_dir}"
 ./copy_dirtree.sh "${config_tree_prefix}/home_files" "${HOME}" minimal user_eeepc
+cat "${repos_list_file}" | while read line; do
+    ensure_repo "${line}"
+done
 curl -fsSl https://raw.githubusercontent.com/tridactyl/tridactyl/78e662efefd1f4af2bdb2a53edecf03b535b997b/native/install.sh | bash
 echo "As tridactyl user, don't forget to do :source on the first Firefox run and then re-start."
