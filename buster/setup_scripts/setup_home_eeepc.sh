c4852aec8669e745e3f4665a6c22d7eecac9034f
Christian Heller <c.heller@plomlompom.de>
2019-05-06 20:11:16 +0200

    More scripts fixing.

--- buster/setup_scripts/setup_home_eeepc.sh
+++ buster/setup_scripts/setup_home_eeepc.sh
@@ -12,6 +12,6 @@ if [ ! -d "${HOME}/${public_repos_dir}/config" ]; then
     git clone https://plomlompom.com/repos/clone/config
 fi
 cd "${setup_scripts_dir}"
-./copy_dirtree.sh "${config_tree_prefix}/home_files" "/root" minimal user_eeepc
+./copy_dirtree.sh "${config_tree_prefix}/home_files" "${HOME}" minimal user_eeepc
 curl -fsSl https://raw.githubusercontent.com/tridactyl/tridactyl/78e662efefd1f4af2bdb2a53edecf03b535b997b/native/install.sh | bash
 echo "As tridactyl user, don't forget to do :source on the first Firefox run and then re-start."
