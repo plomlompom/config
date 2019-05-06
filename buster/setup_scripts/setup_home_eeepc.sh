9981fbe8b1a46056d57b8793af305b0e78c30941
Christian Heller <c.heller@plomlompom.de>
2019-05-06 23:40:52 +0200

    Add borgbackup to home setup.

--- buster/setup_scripts/setup_home_eeepc.sh
+++ buster/setup_scripts/setup_home_eeepc.sh
@@ -2,8 +2,10 @@
 set -e
 
 public_repos_dir="${HOME}/public_repos"
-config_tree_prefix="${public_repos_dir}/config/buster"
-setup_scripts_dir="${config_tree_prefix}/setup_scripts"
+config_tree_prefix="${public_repos_dir}/config"
+path_borgscript="${config_tree_prefix}/all_new_2018/borg.sh"
+config_tree_buster="${config_tree_prefix}/buster"
+setup_scripts_dir="${config_tree_buster}/setup_scripts"
 repos_list_file="${public_repos_dir}/repos"
 dir_secrets="${HOME}/tmp_secrets"
 borgkeys_dir=~/.config/borg/keys
@@ -21,8 +23,9 @@ cd
 mkdir -p "${public_repos_dir}"
 ensure_repo config
 cd "${setup_scripts_dir}"
-./copy_dirtree.sh "${config_tree_prefix}/home_files" "${HOME}" minimal user_eeepc
+./copy_dirtree.sh "${config_tree_buster}/home_files" "${HOME}" minimal user_eeepc
 curl -fsSl https://raw.githubusercontent.com/tridactyl/tridactyl/78e662efefd1f4af2bdb2a53edecf03b535b997b/native/install.sh | bash
+
 cd "${dir_secrets}"
 mkdir -p "${ssh_dir}"
 echo "Setting up .ssh"
@@ -35,6 +38,8 @@ mkdir -p "${borgkeys_dir}"
 mv borg_keyfiles/* "${borgkeys_dir}"
 cd
 rm -rf "${dir_secrets}"
+
+"${path_borgscript}"
 cat "${repos_list_file}" | while read line; do
     ensure_repo "${line}"
 done
